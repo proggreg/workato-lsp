b52583f8903a2c10aeeaf250acbcea30a1906999
Greg Field <greg.field1992@gmail.com>
2026-03-07 21:41:56 +0000

    refactor: improve symbol creation and child symbol retrieval in DocumentParser

--- src/server/documentParser.ts
+++ src/server/documentParser.ts
@@ -13,81 +13,52 @@ export class DocumentParser {
         this.parser.setLanguage(Ruby)
     }
 
-    createSymbol(node: Parser.SyntaxNode, end: Parser.SyntaxNode): DocumentSymbol {
+    createSymbol(keyNode: Parser.SyntaxNode, valueNode: Parser.SyntaxNode): DocumentSymbol {
         return {
-            name: node.text,
+            name: keyNode.text,
             kind: SymbolKind.Method,
-            range: Range.create(node.startPosition.row, node.startPosition.column, node.endPosition.row, node.endPosition.column),
-            selectionRange: Range.create(node.startPosition.row, node.startPosition.column, end.endPosition.row, end.endPosition.column),
+            range: Range.create(keyNode.startPosition.row, keyNode.startPosition.column, valueNode.endPosition.row, valueNode.endPosition.column),
+            selectionRange: Range.create(keyNode.startPosition.row, keyNode.startPosition.column, keyNode.endPosition.row, keyNode.endPosition.column),
             children: [],
             detail: ''
         }
     }
 
+    private getChildSymbols(node: Parser.SyntaxNode): DocumentSymbol[] {
+        if (node.type !== 'hash') return []
+
+        const symbols: DocumentSymbol[] = []
+        for (const child of node.children) {
+            if (child.type === 'pair') {
+                const keyNode = child.childForFieldName('key')
+                const valueNode = child.childForFieldName('value')
+                if (keyNode && valueNode) {
+                    const symbol = this.createSymbol(keyNode, valueNode)
+                    symbol.children = this.getChildSymbols(valueNode)
+                    symbols.push(symbol)
+                }
+            }
+        }
+        return symbols
+    }
+
     getSymbols() {
         if (!this.tree) return []
 
-        const grammer = this.parser.getLanguage()
-        const queryString = `(program (hash (pair 
-                                key: (hash_key_symbol) @hash_key_symbol
-                                value:(_) @value )
-                                ))`
+        const grammar = this.parser.getLanguage()
+        const queryString = `(program (hash (pair
+                                key: (hash_key_symbol) @key
+                                value: (_) @value)))`
 
-        const query = new Query(grammer, queryString)
-        const tree = this.tree
-        const topLevelSymbol: any[] = query.captures(this.tree.rootNode)
+        const query = new Query(grammar, queryString)
+        const captures = query.captures(this.tree.rootNode)
 
-        if (topLevelSymbol.length) {
-            for (let i = 0; i < topLevelSymbol.length; i += 2) {
-                const hash = topLevelSymbol[i]
-                const end = topLevelSymbol[i]
-                const symbol = this.createSymbol(hash.node, end.node)
-                const childrenQueryString = `(program (hash (pair 
-                                            key: (hash_key_symbol) @name (#eq? @name "${hash.node.text}")
-                                            value:
-                                            (hash (pair key:( hash_key_symbol ) @children
-                                                        value: (_) @value
-                                                        )
-                                            ))))
-                                    `
-                const query = new Query(grammer, childrenQueryString)
-                const childCaptures = query.captures(tree.rootNode).filter((c) => c.name !== 'name')
-
-                // TODO use recursion to retreive child symbols
-                if (childCaptures.length) {
-                    const childSymbols = []
-                    for (let i = 0; i < childCaptures.length; i += 2) {
-                        const hash = childCaptures[i]
-                        const end = childCaptures[i]
-
-                        const symbol = this.createSymbol(hash.node, end.node)
-                        const grandChildrenQueryString = `(program (hash (pair 
-                                                        value:
-                                                        (hash (pair key:( hash_key_symbol ) @name (#eq? @name "${hash.node.text}")
-                                                        value: (hash (pair key: (hash_key_symbol) @value
-                                                        )
-                                                        ))))))
-                                    `
-                        const query = new Query(grammer, grandChildrenQueryString)
-                        const grandChildCaptures = query.captures(tree.rootNode).filter((c) => c.name !== 'name')
-                        if (grandChildCaptures.length) {
-                            const children = []
-                            for (let i = 0; i < grandChildCaptures.length; i += 2) {
-                                const hash = grandChildCaptures[i]
-                                const end = grandChildCaptures[i]
-                                const symbol = this.createSymbol(hash.node, end.node)
-                                children.push(symbol)
-                            }
-                            symbol.children = children
-                        }
-
-                        childSymbols.push(symbol)
-                    }
-                    symbol.children = childSymbols
-                }
-
-                this.symbolTable.set(symbol.name, symbol)
-            }
+        for (let i = 0; i < captures.length; i += 2) {
+            const keyCapture = captures[i]
+            const valueCapture = captures[i + 1]
+            const symbol = this.createSymbol(keyCapture.node, valueCapture.node)
+            symbol.children = this.getChildSymbols(valueCapture.node)
+            this.symbolTable.set(symbol.name, symbol)
         }
 
         return Array.from(this.symbolTable.values())
